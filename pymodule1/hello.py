e64367ce2c86ad1d6818a6b0010e05efad57b784
Sunil Wadhwa <sunil.wadhwa01@sap.com>
2018-02-05 16:20:09 -0700

    Added debugging to python module code.

--- pymodule1/hello.py
+++ pymodule1/hello.py
@@ -2,14 +2,20 @@
 
 from flask import Flask
 import os
+import logging
+import sys
 
 app = Flask(__name__)
 
 # Get port from environment variable or choose 9099 as local default
 port = int(os.getenv("PORT", 9099))
 
+# Basic DEBUG logging to stdout
+logging.basicConfig(level=logging.DEBUG, stream=sys.stdout)
+
 @app.route('/')
 def hello_world():
+    logging.debug('Received request for hello_world()')
     return '<html><body> <h1>Hello World!</h1> <p>This is python module pymodule1.</p> </body></html>'
 
 if __name__ == '__main__':
